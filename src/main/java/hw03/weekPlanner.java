70d75232c906adfc7d27667c2b82a1284ef88cfa
Gazanfar <hasanzadegazanfar@gmail.com>
2020-02-13 23:32:13 +0400

    HW3

---
+++ src/main/java/hw03/weekPlanner.java
@@ -0,0 +1,36 @@
+package hw03;
+
+import java.util.Scanner;
+
+public class weekPlanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[][] schedule = new String[7][2];
+        schedule[0][0] = "Sunday";
+        schedule[0][1] = "do home work";
+        schedule[1][0] = "Monday";
+        schedule[1][1] = "go to courses; watch a film";
+
+        String day;
+
+        while(true){
+            System.out.println("Please, input the day of the week:");
+            day = sc.nextLine();
+            day = day.trim();
+            if(day.equals("exit"))
+                break;
+                switch (day) {
+                    case "Sunday":
+                        if (day.equals(schedule[0][0]))
+                            System.out.printf("Your tasks for Sunday: %s \n", schedule[0][1]);
+                    case "Monday":
+                        if (day.equals(schedule[1][0]))
+                            System.out.printf("Your tasks for Monday: %s \n", schedule[1][1]);
+                        break;
+                    default:
+                        System.out.println("Sorry, I don't understand you, please try again.");
+                }
+        }
+    }
+
+}
